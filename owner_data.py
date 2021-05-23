22036f6603b91eb3c5ad840faa655ff4e2652544
sh0TGuN500 <grandma7ter500@gmail.com>
2021-05-23 21:40:56 +0200

    Version 0.2.heroku

---
+++ owner_data.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = '1609142437:AAH2Wgn9uFDkf2rI-kZUXw9GwK4b06cL--I'
+liqpay_sandbox = {'public': '410694247:TEST:c73c1377-1468-413d-92b8-2d975b5fb383', #'632593626:TEST:sandbox_i91885928168',
+                  'private': 'sandbox_rx6P7BJVqqJglXY4u3n1ztPocclrFav0fPQmKPsQ'}
+
+dev = 1545945219
+owner = 1631563895
+
+if __name__ == '__main__':
+    pass
